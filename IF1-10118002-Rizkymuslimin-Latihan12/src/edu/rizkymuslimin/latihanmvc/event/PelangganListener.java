3a24aed196649e1905f17fa5a71200aabcf31c5d
Rizky Muslimin <55826921+Rizky-Muslimin@users.noreply.github.com>
2020-01-07 22:05:41 +0700

    Add files via upload

---
+++ IF1-10118002-Rizkymuslimin-Latihan12/src/edu/rizkymuslimin/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rizkymuslimin.latihanmvc.event;
+
+import edu.rizkymuslimin.latihanmvc.model.PelangganModel;
+
+/**
+ *Nama:Rizky Muslimin
+ *Nim :10118002
+ *Kelas:IF-1
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+}
+
---
+++ IF1-10118002-Rizkymuslimin-Latihan12/src/edu/rizkymuslimin/latihanmvc/model/IF110118002RizkyMusliminLatihan12.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.rizkymuslimin.latihanmvc.model;
+
+import edu.rizkymuslimin.latihanmvc.view.PlangganView;
+
+/**
+ *Nama:Rizky Muslimin
+ *Nim :10118002
+ *Kelas:IF-1
+ */
+public class IF110118002RizkyMusliminLatihan12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        PlangganView view = new PlangganView();
+        view.setVisible(true);
+        
+    }
+    
+}
+
